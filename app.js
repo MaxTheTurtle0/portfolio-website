f1e12996d91b2687836575c4af8a3cdbda423f6a
Marian Antonescu <129096443+MaxTheTurtle0@users.noreply.github.com>
2023-05-21 21:26:46 +0000

    changed the font to Fira Code

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const observer = new  IntersectionObserver((entries) => {
         console.log(entry)
         if (entry.isIntersecting)
         {
-            entry.target.classList.add('show')
+            entry.target.classList.add('show');
         }
         else
         {
-            entry.target.classList.remove('show')
+            entry.target.classList.remove('show');
         }
-    })
-})
+    });
+});
 
 
-const hiddenElements = document.querySelector('.hidden')
-hiddenElements.forEach((el) => observer.observe(el))
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
