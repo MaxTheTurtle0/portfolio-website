936898f25d93986be9d57033d33d79a2a2883b42
Andrei <m.andrei.antonescu@gmail.com>
2023-05-22 16:52:32 +0200

    Added navbar

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 const observer = new  IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
         if (entry.isIntersecting)
         {
-            entry.target.classList.add('show');
+            entry.target.classList.add("show");
         }
         else
         {
-            entry.target.classList.remove('show');
+            entry.target.classList.remove("show");
         }
     });
 });
 
 
-const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+const navigationbar = document.querySelector(".navigationbar");
+const mobile_burger_menu = document.querySelector(".mobile-burger-menu");
+
+mobile_burger_menu.addEventListener("click", () => {
+    const isVisible = navigationbar.getAttribute("data-visible");
+    if (isVisible === "false")
+    {
+        navigationbar.setAttribute("data-visible", "true");
+        mobile_burger_menu.setAttribute("aria-expanded", "true");
+    }
+    else
+    {
+        navigationbar.setAttribute("data-visible", "false");
+        mobile_burger_menu.setAttribute("aria-expanded", "false")
+    }
+});
