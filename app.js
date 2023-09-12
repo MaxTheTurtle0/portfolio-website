8cb5c3b37c53f4d3c9bc2917af574072927e120a
MaxTheTurtle0 <m.andrei.antonescu@gmail.com>
2023-09-12 23:34:25 +0200

    moved from a js burger menu to a no js burger menu and added an animation for the burger menu

--- app.js
+++ app.js
@@ -13,21 +13,6 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
 
-const navigationbar = document.querySelector(".navigationbar");
-const mobile_burger_menu = document.querySelector(".mobile-burger-menu");
-
-mobile_burger_menu.addEventListener("click", () => {
-    const is_visible = navigationbar.getAttribute("data-visible");
-    if (is_visible === "false") {
-        navigationbar.setAttribute("data-visible", "true");
-        mobile_burger_menu.setAttribute("aria-expanded", "true");
-    } else {
-        navigationbar.setAttribute("data-visible", "false");
-        mobile_burger_menu.setAttribute("aria-expanded", "false");
-    }
-});
-
-
 const description = ["software engineer.", "student.", "gamer.", "musician."];
 let descriptionElement = document.getElementById("description");
 
