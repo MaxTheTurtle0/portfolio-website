3f8d082356dc4844dd881923177096b7221cf2a9
MaxTheTurtle0 <m.andrei.antonescu@gmail.com>
2023-10-11 22:15:12 +0200

    fixed an overflow regarding the h1 on smaller screens and changed the js a bit

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show");
-        }
-        else {
-            entry.target.classList.remove("show");
-        }
-    });
+    entries.forEach((entry) => entry.target.classList.toggle("show", entry.isIntersecting));
 });
 
-
 const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => observer.observe(el));
+hiddenElements.forEach((element) => observer.observe(element));
 
 const description = ["software engineer.", "student.", "gamer.", "musician."];
-let descriptionElement = document.getElementById("description");
+const descriptionElement = document.getElementById("description");
 
 let j = 0;
 
@@ -29,12 +21,11 @@ const typeWriter = (text, element, delay) => {
         }
     }
     j++;
+ 
     if (j >= description.length) j = 0;
     type();
 };
 
 typeWriter(description[j], descriptionElement, 75);
 
-setInterval(() => {
-    typeWriter(description[j], descriptionElement, 75);
-}, 4500);
+setInterval(() => typeWriter(description[j], descriptionElement, 75), 4500);
