4cd05beaf62be94177ee9994a38d1e64b62c2777
MaxTheTurtle0 <m.andrei.antonescu@gmail.com>
2024-01-26 19:14:37 +0100

    a lot of changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ const defaultTheme = require("tailwindcss/defaultTheme")
                     primary: "#202731",
                     intro: "#ee6e3d",
                     projects: "#3c31dd",
-                    tools: "#00caca",
+                    tools: "#008090",
                 },
             },
         },
