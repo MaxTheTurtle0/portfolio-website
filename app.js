f64c7f7950de6fda1249b5baa8c466ab01e90a6b
MaxTheTurtle0 <m.andrei.antonescu@gmail.com>
2024-01-27 14:02:45 +0100

    refactor: add new observer with transition delay for the svgs in tools section, add more svgs and add text for tools section

--- app.js
+++ app.js
@@ -2,6 +2,15 @@
 const observer = new IntersectionObserver((entries) => entries.forEach((entry) => entry.target.classList.toggle("show", entry.isIntersecting)));
 document.querySelectorAll(".hidden-transition").forEach((element) => observer.observe(element));
 
+// fade in svg elements when they are in view
+const svgObserver = new IntersectionObserver((entries) => {
+    for (let i = 0; i < entries.length; i++) {
+        entries[i].target.classList.toggle("show", entries[i].isIntersecting) 
+        entries[i].target.style.transitionDelay = `${i * 0.05}s`;
+    }
+});
+document.querySelectorAll(".hidden-transition-svg").forEach((element) => svgObserver.observe(element));
+
 // Typewriter effect
 const description = ["software engineer.", "student.", "gamer.", "musician."];
 const descriptionElement = document.getElementById("description");
